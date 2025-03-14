acd394070b2668507308e328bb8b61907454c2ba
boroboro01 <duke7201@naver.com>
2025-03-14 20:46:58 +0900

    create form for book item

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    private String title;
+    private int price;
+    private int stockQuantity;
+
+    // 책을 위한 속성
+    private String author;
+    private String isbn;
+}
