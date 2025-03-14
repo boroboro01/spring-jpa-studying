6c3df331bc94bcee9b3a2aaa740824fb15f269a1
boroboro01 <duke7201@naver.com>
2025-03-14 21:00:31 +0900

    create a factory method for a book

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -3,12 +3,12 @@ package jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
 public class BookForm {
 
     private Long id;
 
-    private String title;
+    private String name;
     private int price;
     private int stockQuantity;
 
--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -2,8 +2,11 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jpabook.jpashop.domain.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @DiscriminatorValue("Book")
@@ -11,5 +14,25 @@ import lombok.Setter;
 public class Book extends Item {
 
     private String author;
-    private String title;
+    private String isbn;
+
+    /**
+     * Book Entity를 생성하는 팩토리 메서드
+     * @param name
+     * @param price
+     * @param stockQuantity
+     * @param author
+     * @param isbn
+     * @return
+     */
+    public static Book createBook(String name, int price, int stockQuantity, String author, String isbn) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        book.setAuthor(author);
+        book.setIsbn(isbn);
+        return book;
+    }
+
 }
